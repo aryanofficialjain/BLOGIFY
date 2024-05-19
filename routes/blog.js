05990da6cd43b38179dc02e3bfe2b1459f982177
Aryan Jain <aryanofficialjain@gmail.com>
2024-05-19 11:14:58 +0530

    completed the blog app aith login signup addblog seeblog with thumbnail and image on blog post

--- routes/blog.js
+++ routes/blog.js
@@ -26,6 +26,14 @@ router.get('/add-new', (req,res) => {
     })
 })
 
+router.get('/:id', async (req,res) => {
+    const blog = await Blog.findById(req.params.id)
+    return res.render("Blog", {
+        user: req.user,
+        blog,
+    })
+})
+
 
 
 router.post('/', upload.single("coverImage"), async(req,res) => {
@@ -42,7 +50,7 @@ router.post('/', upload.single("coverImage"), async(req,res) => {
     })    
     console.log(blog);
 
-    return res.redirect(`/blog${blog._id}`);
+    return res.redirect(`/blog/${blog._id}`);
 
 
 
